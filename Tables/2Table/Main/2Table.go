787a83e499785d0894d2fb3113471c6b38124acd
SageCore <Muhammad.Raahim@gmail.com>
2017-09-06 18:37:54 +0500

    Table tansition

---
+++ Tables/2Table/Main/2Table.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 11; i++ {
+		fmt.Printf("2 * %d = %d\n", i, i*2)
+	}
+	fmt.Println("2's table done\n")
+
+	for i := 0; i < 11; i++ {
+		fmt.Printf("3 * %d = %d\n", i, i*3)
+	}
+	fmt.Println("3's table done \n")
+
+	for i := 0; i < 11; i++ {
+		fmt.Printf("4 * %d = %d\n", i, i*4)
+	}
+	fmt.Println("4's table done \n")
+
+	for i := 0; i < 11; i++ {
+		fmt.Printf("5 * %d = %d\n", i, i*5)
+	}
+	fmt.Println("5's table done \n")
+
+	for i := 0; i < 11; i++ {
+		fmt.Printf("6 * %d = %d\n", i, i*6)
+	}
+	fmt.Println("6's table done \n")
+
+	for i := 0; i < 11; i++ {
+		fmt.Printf("7 * %d = %d\n", i, i*7)
+	}
+	fmt.Println("7's table done \n")
+
+	for i := 0; i < 11; i++ {
+		fmt.Printf("8 * %d = %d\n", i, i*8)
+	}
+	fmt.Println("8's table done \n")
+
+	for i := 0; i < 11; i++ {
+		fmt.Printf("9 * %d = %d\n", i, i*9)
+	}
+	fmt.Println("9's table done \n")
+
+	for i := 0; i < 11; i++ {
+		fmt.Printf("10 * %d = %d\n", i, i*10)
+	}
+	fmt.Println("10's table done\n")
+
+}
